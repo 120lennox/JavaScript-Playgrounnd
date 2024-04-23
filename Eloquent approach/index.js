4d5412a75829a2b7d21e3fdb2bda2a14cd1d9ead
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-23 22:33:13 +0200

    string comparison

---
+++ Eloquent approach/index.js
@@ -0,0 +1,11 @@
+//comparison in javascript
+
+/**
+ * strings can be compared in JS. upper case letters are always less than lower case letters.
+ * first alphabet letters are less than latter letters  
+ */
+
+console.log("Lennox" < "lennox"); //returns true
+console.log("aaron" < "baron"); //returns true
+console.log("mountain" < "mountains"); //returns true. mountains has more characters
+
