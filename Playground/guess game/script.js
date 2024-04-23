90e47d7e9553d1669b98b3acbd67a136623433e4
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-23 21:14:04 +0200

    javaScript we start again stronger

--- Playground/guess game/script.js
+++ Playground/guess game/script.js
@@ -1,7 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-const guesses = document.querySelector('guesses');
-const lastResult = document.querySelector('lastResult');
-const lowOrHi = document.querySelector('lowOrHi');
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
@@ -14,7 +14,7 @@ function checkguess(){
     if (guessCount === 1){
         guesses.textContent = "Previous guesses: ";
     }
-    guesses.textContent = '${guesses.textContent} ${userGuess}';
+    guesses.textContent = `${guesses.textContent} ${userGuess}`;
 
     if (userGuess === randomNumber){
         lastResult.textContent = "Congratulations! thats the number";
@@ -30,14 +30,56 @@ function checkguess(){
         setGameOver();
     }
 
-    else if (guessCount < 10){
+    else {
         lastResult.textContent = "Wrong!";
         lastResult.style.backgroundColor = 'red';
 
-        if (userGuess <)
+        if (userGuess < randomNumber){
+            lowOrHi.textContent = "Number too low!";
+        }
+        else if(userGuess > randomNumber){
+            lowOrHi.textContent = "Number too high";
+        } 
     }
 
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
     
 }
+
+guessSubmit.addEventListener("click", checkguess);
+
+function setGameOver(){
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start a new Game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame(){
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for (resetPara of resetParas){
+        resetParas.textContent = "";
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    lastResult.style.backgroundColor = "white";
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    checkguess(); // Call the checkguess function
+});
+
 
 
