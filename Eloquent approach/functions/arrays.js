592c0f68d34e88f1eeb0625fb5f957186ca97ba0
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-30 15:43:47 +0200

    Javascripts objects start

--- Eloquent approach/functions/arrays.js
+++ Eloquent approach/functions/arrays.js
@@ -1,3 +1,10 @@
 //accessing arrays
-const numbers = [x, 2, 3, 4]
-console.log(numbers.x)
+const numbers = ["x", 2, 3, 4]
+const names = ["Lennox", "dell", "Jacques", 2, 4]
+// console.log(numbers.x)
+
+//array length
+// console.log(numbers.length)
+// console.log(names["Lennox"])
+// console.log(names.length)
+// console.log(names[0])
