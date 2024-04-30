63f3c0264596030634a7a85c787367349e4e5aa6
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-30 21:32:32 +0200

    alert function updated

--- Eloquent approach/functions/functions.js
+++ Eloquent approach/functions/functions.js
@@ -1,10 +1,17 @@
-let heading = document.querySelector("h1");
-function change(){
-    console.log("button clicked");
-    if (heading.textContent === "Hey!"){
-        heading.innerHTML = "Lennox Start coding";
-    }
-    else{
-        heading.innerHTML = "text dont match";
-    }
+// let heading = document.querySelector("h1");
+// function change(){
+//     console.log("button clicked");
+//     if (heading.textContent === "Hey!"){
+//         heading.innerHTML = "Lennox Start coding";
+//     }
+//     else{
+//         heading.innerHTML = "text dont match";
+//     }
+// }
+
+let heading = document.querySelector(".heading")
+
+function clicked(){
+    let name = prompt("Name: ")
+    heading.textContent = `Hello ${name}`
 }
