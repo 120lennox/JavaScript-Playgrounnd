ccfe0c1785bd34de1812380468eb565f7a5b8846
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-30 20:48:42 +0200

    working with methods

--- Eloquent approach/functions/arrays.js
+++ Eloquent approach/functions/arrays.js
@@ -1,3 +1,4 @@
+//commented code is not buggy, just dont want it to display anything on console
 //accessing arrays
 const numbers = ["x", 2, 3, 4]
 const names = ["Lennox", "dell", "Jacques", 2, 4]
@@ -16,10 +17,15 @@ let tasks = {
 }
 
 for (let i = 0; i < tasks.smaller_tasks.length; i++){
-    console.log(tasks.smaller_tasks[i])
+    // console.log(tasks.smaller_tasks[i])
 }
 
-console.log(tasks.smaller_tasks[0])
-tasks.smaller_tasks.push("pushed..")
+// console.log(tasks.smaller_tasks[0])
+// tasks.smaller_tasks.push("pushed..")
+
+// in operator 
+console.log("wash" in tasks.smaller_tasks)
+delete tasks.lager_tasks
+console.log("larger_tasks" in tasks)
 
 
