98a6b61ef12b6723f134343574006bfc8d8f94f4
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 13:05:07 +0200

    code works

--- lecture slides/events/event.js
+++ lecture slides/events/event.js
@@ -1,13 +1,18 @@
-const change_btn = document.querySelector(".button1")
-const reset_btn = document.querySelector(".button2")
+const change_btn = document.querySelector(".button1");
+const reset_btn = document.querySelector(".button2");
 
 function random_number(number){
-    return Math.floor(Math.random * (number + 1))
+    return Math.floor(Math.random() * (number + 1));
 }
 
 function set_color(){
-    const random_color = `rgb(${random_number(255)}, ${random_number(255)}, ${random_number(255)})`
-    document.body.style.backgroundColor = random_color
+    const random_color = `rgb(${random_number(255)}, ${random_number(255)}, ${random_number(255)})`;
+    document.body.style.backgroundColor = random_color;
 }
 
-change_btn.addEventListener('click', set_color)
+function reset_color(){
+    document.body.style.backgroundColor = "initial";
+}
+
+change_btn.addEventListener('click', set_color);
+reset_btn.addEventListener('click', reset_color);
