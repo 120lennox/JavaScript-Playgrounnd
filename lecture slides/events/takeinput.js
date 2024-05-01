622d4cc8724af1acb3866bc4c2838916a849a977
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 22:53:04 +0200

    this code works fine

--- lecture slides/events/takeinput.js
+++ lecture slides/events/takeinput.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const num1 = document.querySelector("#num1")
     const num2 = document.querySelector("#num2")
     let heading = document.querySelector("h2")
-    const sub = document.querySelector(".sumbit")
+    const sub = document.querySelector(".submit")
 
     const calculate = ()=>{
         return new Promise((resolve, reject) => {
