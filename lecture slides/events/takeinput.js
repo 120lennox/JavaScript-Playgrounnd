d2b5cb3efe83025614f266cff5dc8886e4a40edb
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 23:43:22 +0200

    tried async and awai tusing same code

--- lecture slides/events/takeinput.js
+++ lecture slides/events/takeinput.js
@@ -1,28 +1,54 @@
+// commented code is not buggy
+
+// document.addEventListener("DOMContentLoaded", ()=>{
+    // const num1 = document.querySelector("#num1")
+    // const num2 = document.querySelector("#num2")
+    // let heading = document.querySelector("h2")
+    // const sub = document.querySelector(".submit")
+
+//     const calculate = ()=>{
+//         return new Promise((resolve, reject) => {
+//             //asynchronous task
+//             const result = parseFloat(num1.value) + parseFloat(num2.value)
+//             if (isNaN(result)){
+//                 reject("invalid input")
+//             }
+//             else{
+//                 resolve(result)
+//             }
+//         })
+//     }
+
+//     sub.addEventListener("click", ()=>{
+//         calculate().then((result) =>{
+//             heading.innerHTML = `answer is ${result}`
+//         }).catch((error) =>{
+//             heading.innerHTML =`error: ${error}`
+//         })
+//     })
+// })
+
+// implementing the same using async and await
 document.addEventListener("DOMContentLoaded", ()=>{
     const num1 = document.querySelector("#num1")
     const num2 = document.querySelector("#num2")
     let heading = document.querySelector("h2")
     const sub = document.querySelector(".submit")
 
-    const calculate = ()=>{
-        return new Promise((resolve, reject) => {
-            //asynchronous task
-            const result = parseFloat(num1.value) + parseFloat(num2.value)
+    async function calculate(){
+        try{
+            const result = await parseFloat(num1) + parseFloat(num2)
             if (isNaN(result)){
-                reject("invalid input")
+                heading.innerHTML = "invalid input"
             }
             else{
-                resolve(result)
+                heading.innerHTML = `Answer: ${result}`
             }
-        })
+        }
+        catch(error){
+            heading.innerHTML = error
+        }
     }
-
-    sub.addEventListener("click", ()=>{
-        calculate().then((result) =>{
-            heading.innerHTML = `answer is ${result}`
-        }).catch((error) =>{
-            heading.innerHTML =`error ${error}`
-        })
-    })
 })
+
 
