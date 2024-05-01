04348b23abdf062b65935663a4f51220fa3f5102
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 15:38:27 +0200

    tried a promise code

--- lecture slides/events/asynchronous.js
+++ lecture slides/events/asynchronous.js
@@ -1,3 +1,5 @@
+//COMMENTED CODE IS NOT BUGGY
+
 // setTimeout(() => console.log("Hello world first"), 5000);
 // console.log("Hello world second!")
 
@@ -7,9 +9,23 @@
 
 //Callbacks
 //These are functions thats are passed as arguments to other functions. these functions get executed once the outer function gets executed. refer example below. 
-setTimeout(function(){
-    alert("Executed after first function executes")
-}, 6000)
+// setTimeout(function(){
+//     alert("Executed after first function executes")
+// }, 6000)
 //function inside setTimeout executes after setTimeout excutes
 
 //Promises
+const myPromise = new Promise((resolve, reject) =>{
+    //perfom asynchronous operation
+    setTimeout(() =>{
+        resolve("Success")
+    }, 5000)
+})
+//handling promises
+myPromise.then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.error(error)
+})
+
+
