5703950b8a637fc914678ee085128d6dcd638534
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 22:47:28 +0200

    tried  implementation with promises

--- lecture slides/events/takeinput.js
+++ lecture slides/events/takeinput.js
@@ -1,11 +1,28 @@
-const num1 = document.querySelector("#num1")
-const num2 = document.querySelector("#num2")
-let heading = document.querySelector("h2")
-const sub = document.querySelector(".sumbit")
+document.addEventListener("DOMContentLoaded", ()=>{
+    const num1 = document.querySelector("#num1")
+    const num2 = document.querySelector("#num2")
+    let heading = document.querySelector("h2")
+    const sub = document.querySelector(".sumbit")
 
-const calculate = () => {
-    const result = parseInt(num1.value )+ parseInt(num2.value)
-    heading.innerHTML = result
-}
+    const calculate = ()=>{
+        return new Promise((resolve, reject) => {
+            //asynchronous task
+            const result = parseFloat(num1.value) + parseFloat(num2.value)
+            if (isNaN(result)){
+                reject("invalid input")
+            }
+            else{
+                resolve(result)
+            }
+        })
+    }
 
-sub.addEventListener("click", calculate)
+    sub.addEventListener("click", ()=>{
+        calculate().then((result) =>{
+            heading.innerHTML = `answer is ${result}`
+        }).catch((error) =>{
+            heading.innerHTML =`error ${error}`
+        })
+    })
+})
+
