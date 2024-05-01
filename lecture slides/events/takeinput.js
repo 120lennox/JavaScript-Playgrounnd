19fba84460d9f2b4436cc7f4a5328607d3012621
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 22:09:28 +0200

    new program

---
+++ lecture slides/events/takeinput.js
@@ -0,0 +1,11 @@
+const num1 = document.querySelector("#num1")
+const num2 = document.querySelector("#num2")
+let heading = document.querySelector("h2")
+const sub = document.querySelector(".sumbit")
+
+function calculate(){
+    return num1.value + num2.value 
+}
+
+
+
