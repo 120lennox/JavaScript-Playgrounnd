635ceb4ad12217f376083454d1863d34abce94e0
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-01 11:36:30 +0200

    no color changing, dont know why yet to figure out

--- lecture slides/events/event.js
+++ lecture slides/events/event.js
@@ -1,2 +1,13 @@
 const change_btn = document.querySelector(".button1")
 const reset_btn = document.querySelector(".button2")
+
+function random_number(number){
+    return Math.floor(Math.random * (number + 1))
+}
+
+function set_color(){
+    const random_color = `rgb(${random_number(255)}, ${random_number(255)}, ${random_number(255)})`
+    document.body.style.backgroundColor = random_color
+}
+
+change_btn.addEventListener('click', set_color)
