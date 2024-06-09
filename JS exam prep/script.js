ddbd5c05aa2d046fa41f89e45718fe59e2a0873a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-06-09 10:39:41 +0200

    dom manipulation revision

---
+++ JS exam prep/script.js
@@ -0,0 +1,8 @@
+//ADD ELEMENT ON DOM
+const head = document.createElement("h1")
+head.textContent = "Js exam prep"
+document.querySelector("section").appendChild(head)
+
+//DOM MANIPULATION
+head.style.alignSelf = "center"
+head.style.color = "green"
--- lecture slides/events/DOM-MANIPULATION/dom.js
+++ lecture slides/events/DOM-MANIPULATION/dom.js
@@ -1,4 +1,28 @@
 //creating an element in DOM
 const para = document.createElement("h1")
 para.textContent = "Text created"
-document.querySelector("section").append(para)
+setTimeout(()=>(document.querySelector("section").append(para)), 5000)
+console.log("10 sec to append message")
+
+//perfom same operation using promises
+const myPromise = new Promise((resolve, reject) =>
+    //first function for promise
+{
+    //second function to perfom asynchronous operation
+    setTimeout(()=>{
+        resolve("success")
+    }, 10000)
+})
+
+myPromise.then((result) =>{
+    console.log(result)
+}).catch((error) => {
+    console.error(error)
+})
+
+async function setAlarm(name, delay){
+    try{
+        const alarmPromise = alarm(name, delay)
+        
+    }
+}
